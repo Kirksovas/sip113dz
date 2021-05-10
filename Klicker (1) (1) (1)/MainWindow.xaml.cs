0a80d7adc678e207077de4728faaf7f7734170ee
Kirksovas <mettarisk23@gmail.com>
2021-05-11 00:34:45 +0300

    cliker
    cliker

---
+++ Klicker (1) (1) (1)/MainWindow.xaml.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Klicker
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        long point = 300;
+        int ppc = 1;
+        double sol_b1;
+        double sol_b2;
+        double sol_b3;
+        List<ListRab> LR = new List<ListRab>
+        {
+            new ListRab(){Name = "Раб Боб", Price = 15800000},
+            new ListRab(){Name = "Рабыня Сабина", Price = 1300},
+            new ListRab(){Name = "Раб Биба", Price = 151552},
+            new ListRab(){Name = "Раб Илья", Price = 99999000},
+            new ListRab(){Name = "Раб Абоба", Price = 100005}
+
+        };
+
+        List<Tools> Tool = new List<Tools>
+        {
+            new Tools(){Nameto = "Йоршик", Priceto = 120},
+            new Tools(){Nameto = "Почисулкин", Priceto = 18002},
+            new Tools(){Nameto = "ВкусноКорм", Priceto = 99999999},
+            new Tools(){Nameto = "Мячик", Priceto = 10000},
+            new Tools(){Nameto = "Кошачья Мята", Priceto = 1555000}
+
+        };
+
+
+        private void Update()
+        {
+            pointlb.Content = $"Point: {point}";
+            ppclb.Content = $"Point on click: {ppc}";
+            double sol_b1 = 10 + 10 * (30 + ppc * 0.2);
+            double sol_b2 = 20 + 20 * (60 + ppc * 0.4);
+            double sol_b3 = 30 + 30 * (90 + ppc * 0.6);
+
+            b1.Content = (sol_b1).ToString();
+            b2.Content = (sol_b2).ToString();
+            b3.Content = (sol_b3).ToString();
+        }
+        public void Timer_Tick(object sender, EventArgs e)
+        {
+            point += ppc;
+            Update();
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            sol_b1 = 10 + 10 * (30 + ppc * 0.2);
+            sol_b2 = 20 + 20 * (60 + ppc * 0.4);
+            sol_b3 = 30 + 30 * (90 + ppc * 0.6);
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+            cbr.ItemsSource = LR;
+            tooly.ItemsSource = Tool;
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            point += ppc;
+
+            Update();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (point >= (sol_b1))
+            {
+                point -=  Convert.ToInt64(Math.Round(sol_b1));
+                ppc += 3;
+                Update();
+
+            }
+            
+
+        }
+
+        private void ex_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void b2_Click(object sender, RoutedEventArgs e)
+        {
+            if (point >= (sol_b2))
+            {
+                point -= Convert.ToInt64(Math.Round(sol_b2));
+                ppc += 6;
+                Update();
+
+            }
+        }
+
+        private void b3_Click(object sender, RoutedEventArgs e)
+        {
+            if (point >= (sol_b3))
+            {
+                point -= Convert.ToInt64(Math.Round(sol_b3));
+                ppc += 9;
+                Update();
+
+            }
+        }
+
+
+        private void BBUY_Click(object sender, RoutedEventArgs e)
+        {
+            ListRab f = cbr.SelectedItem as ListRab;
+            if (f is null)
+                return;
+            if(point >= f.Price)
+            {
+                tb_purchases.AppendText(f.Name + "\n");
+                point -= f.Price;
+            }
+
+
+        }
+
+        private void Baybuy_Click(object sender, RoutedEventArgs e)
+        {
+            Tools t = tooly.SelectedItem as Tools;
+            if (t is null)
+                return;
+            if(point >= t.Priceto)
+            {
+                tb_purchases.AppendText(t.Nameto + "\n");
+                point -= t.Priceto;
+            }
+        }
+
+        private void tooly_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cbr_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cbr_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+
+    public class ListRab
+    {
+         public string Name { get; set;  }
+        public int Price { get; set;  }
+    }
+    public class Tools
+    {
+        public string Nameto { get; set; }
+        public int Priceto { get; set; }
+    }
+}
---
+++ Klicker (1) (1) (1)/Zadanie_2/Zadanie_2/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string pass = "kukuha";
+        public MainWindow()
+        {
+            InitializeComponent();
+            lb3.Visibility = Visibility.Hidden;
+            lb4.Visibility = Visibility.Hidden;
+            lb5.Visibility = Visibility.Hidden;
+            lb2.Visibility = Visibility.Hidden;
+            date.Visibility = Visibility.Hidden;
+            lib1.Visibility = Visibility.Hidden;
+            tb2.Visibility = Visibility.Hidden;
+            tb1.Visibility = Visibility.Hidden;
+            bt2.Visibility = Visibility.Hidden;
+            bt3.Visibility = Visibility.Hidden;
+            bt4.Visibility = Visibility.Hidden;
+            bt5.Visibility = Visibility.Hidden;
+      
+        }
+
+        private void bt3_Click(object sender, RoutedEventArgs e)
+        {
+            lb1.IsEnabled = true;
+        }
+
+        private void tb1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void bt_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            if (tb_password.Text == pass)
+
+            {
+
+                lb1.Visibility = Visibility.Hidden;
+                tb_password.Visibility = Visibility.Hidden;
+                bt.Visibility = Visibility.Hidden;
+                lb3.Visibility = Visibility.Visible;
+                lb4.Visibility = Visibility.Visible;
+                lb5.Visibility = Visibility.Visible;
+                lb2.Visibility = Visibility.Visible;
+                date.Visibility = Visibility.Visible;
+                lib1.Visibility = Visibility.Visible;
+                tb2.Visibility = Visibility.Visible;
+                tb1.Visibility = Visibility.Visible;
+                bt2.Visibility = Visibility.Visible;
+                bt3.Visibility = Visibility.Visible;
+                bt4.Visibility = Visibility.Visible;
+                bt5.Visibility = Visibility.Visible;
+
+            }
+            else
+            {
+                tb_password.BorderBrush = Brushes.Red;
+                MessageBox.Show("Не верный пароль!");
+            }
+        }
+
+        private void lib1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           
+        }
+
+        private void bt4_Click(object sender, RoutedEventArgs e)
+        {
+            tb1.AppendText(tb2.Text + Environment.NewLine);
+        }
+
+        private void bt2_Click(object sender, RoutedEventArgs e)
+        {
+            date_ValueChanged();
+        }
+
+        private void date_ValueChanged()
+        {
+            lb_date.Content =  date.DisplayDate;
+            date.Visibility = Visibility.Hidden;
+
+        }
+
+        private void bt5_Click(object sender, RoutedEventArgs e)
+        {
+            string text = ((ListBoxItem)lib1.SelectedItem).Content.ToString();
+
+            tb1.AppendText(text + Environment.NewLine);
+        }
+
+        private void tb2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void date_ValueChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+    }
+}
